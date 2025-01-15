aaeca0143c36f52d3a2b151c5848f109e93bb32c
Atlas-Lofton <gll4392@Hotmail.com>
2025-01-15 11:33:57 -0600

    updated index expirements

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*let healthPoints = 4;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}*/
+
+    /*let delivery = 8;
+
+    delivery = 12;
+    delivery = 25;
+    if (delivery <= 10) {
+        console.log("Delivery is early")
+    }
+    else if (delivery >10 && delivery < 24){
+        console.log("Delivery is late.")
+    }
+    else {
+        console.log("There has been an error scheduling your delivery.")
+    } */
+
+        
